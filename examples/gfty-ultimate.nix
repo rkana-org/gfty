a11b8d7207221bb64b00a828821be106acf56f98
oddlama <oddlama@oddlama.org>
2026-07-30 18:21:13 +0200

    feat: extract gfty-ultimate config

---
+++ examples/gfty-ultimate.nix
@@ -0,0 +1,45 @@
+xUnits: yUnits: {
+  base_enable = true;
+  base_magnets_connector_cutouts_enable = true;
+  base_magnets_connector_pin_enable = true;
+  base_magnets_enable = true;
+  base_rounded_corners_enable = false;
+  bin_enable = true;
+  bin_nesting_enable = true;
+  bin_nesting_swappable_rim_enable = true;
+  bin_nesting_swappable_rim_spring_compensation_enable = true;
+  bin_tub_easygrab_enable = true;
+  bin_tub_enable = true;
+  bin_tub_label_depth = "0.01 meter";
+  bin_tub_label_enable = true;
+  bin_tub_label_is_fullwidth = true;
+  bin_tub_label_is_swappable = true;
+  bin_tub_label_width_units = 1;
+  max_print_overhang = "60 deg";
+  bin_nesting_swappable_rim_spring_compensation_additional_rim_expansion = "0 meter";
+  size_x_units = xUnits;
+  size_y_units = yUnits;
+  size_z_units = 6;
+  bin_tub_divider_config = {
+    columns = [ "auto" ];
+    rows = [ "auto" ];
+    merges = [ ];
+    easygrab = [
+      {
+        side = "south";
+        cols = [
+          0
+          0
+        ];
+        rows = [
+          0
+          0
+        ];
+        radius = "21 mm";
+      }
+    ];
+  };
+  bin_tub_label_swappable_supports_enable = true;
+  bin_tub_label_swappable_embossing_clearance = "0.0004 meter";
+  bin_tub_label_swappable_embossing_inset_height = "0.0 meter";
+}
--- examples/labels.nix
+++ examples/labels.nix
@@ -1,60 +1,17 @@
 let
-  gftyUltimateConfig = {
-    base_enable = true;
-    base_magnets_connector_cutouts_enable = true;
-    base_magnets_connector_pin_enable = true;
-    base_magnets_enable = true;
-    base_rounded_corners_enable = false;
-    bin_enable = true;
-    bin_nesting_enable = true;
-    bin_nesting_swappable_rim_enable = true;
-    bin_nesting_swappable_rim_spring_compensation_enable = true;
-    bin_tub_easygrab_enable = true;
-    bin_tub_enable = true;
-    bin_tub_label_depth = "0.01 meter";
-    bin_tub_label_enable = true;
-    bin_tub_label_is_fullwidth = true;
-    bin_tub_label_is_swappable = true;
-    bin_tub_label_width_units = 1;
-    max_print_overhang = "60 deg";
-    bin_nesting_swappable_rim_spring_compensation_additional_rim_expansion = "0 meter";
-    size_x_units = 1;
-    size_y_units = 1;
-    size_z_units = 6;
-    bin_tub_divider_config = {
-      columns = [ "auto" ];
-      rows = [ "auto" ];
-      merges = [ ];
-      easygrab = [
-        {
-          side = "south";
-          cols = [
-            0
-            0
-          ];
-          rows = [
-            0
-            0
-          ];
-          radius = "21 mm";
-        }
-      ];
-    };
-    bin_tub_label_swappable_supports_enable = true;
-    bin_tub_label_swappable_embossing_clearance = "0.0004 meter";
-    bin_tub_label_swappable_embossing_inset_height = "0.0 meter";
-  };
+  gftyUltimate = import ./gfty-ultimate.nix;
 in
 {
   perSystem = {
     gfty-label = {
       labels.module-example = {
+        gfty-ultimate = gftyUltimate 1 1;
         template = ./templates/1x1.svg;
         text.top = "Hi";
         text.bottom = "Hi";
-        gfty-ultimate = gftyUltimateConfig;
       };
       plates.module-example = {
+        gfty-ultimate = gftyUltimate 1 1;
         dimensions = [
           "100mm"
           "50mm"
@@ -63,7 +20,6 @@ in
           "module-example"
           "module-example"
         ];
-        gfty-ultimate = gftyUltimateConfig;
       };
     };
   };
